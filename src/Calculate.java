71b95071b511e10b69a46ff0d1bf1090d6a507d8
Sogerton <sojerton@gmail.com>
2025-07-13 12:52:30 +0300

    console calc +loop+check

--- src/Calculate.java
+++ src/Calculate.java
@@ -2,22 +2,44 @@ import java.util.Scanner;
 
 public class Calculate {
     public static void main(String[] args) {
-        System.out.print("Введите число 1 - ");
-        double a = new Scanner(System.in).nextDouble();
+        boolean count = true;
+        while (count) {
 
-        System.out.print("Введите действие (+-*/%) - ");
-        char d = new Scanner(System.in).nextLine().charAt(0);
+            System.out.print("Введите число 1 - ");
+            double a = new Scanner(System.in).nextDouble();
 
-        System.out.print("Введите число 2 - ");
-        double b = new Scanner(System.in).nextDouble();
+            System.out.print("Введите действие (+-*/%) - ");
+            char d = new Scanner(System.in).nextLine().charAt(0);
 
-        switch (d) {
-            case '+': System.out.print("Результат сложения = " + (a + b));break;
-            case '-': System.out.print("Результат вычитания = " + (a - b));break;
-            case '*': System.out.print("Результат умножения = " + (a * b));break;
-            case '/': System.out.print("Результат деления = " + (a / b));break;
-            case '%': System.out.print("Результат остатка от деления = " + (a % b));break;
+            System.out.print("Введите число 2 - ");
+            double b = new Scanner(System.in).nextDouble();
+
+            switch (d) {
+                case '+':
+                    System.out.println("Результат сложения = " + (a + b));
+                    break;
+                case '-':
+                    System.out.println("Результат вычитания = " + (a - b));
+                    break;
+                case '*':
+                    System.out.println("Результат умножения = " + (a * b));
+                    break;
+                case '/':
+                    System.out.println("Результат деления = " + (a / b));
+                    break;
+                case '%':
+                    System.out.println("Результат остатка от деления = " + (a % b));
+                    break;
+            }
+            System.out.print("Хотите продолжить? (+ -) ");
+            char scan = new Scanner(System.in).nextLine().charAt(0);
+            if (scan == '+') {
+                count = true;
+            } else if (scan == '-') {
+                count = false;
+            } else {
+                System.out.println("ошибка ввода!");
+            }
         }
-
     }
 }
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,6 +5,14 @@ public class HelloWorld {
         System.out.println("Hello World!");
         int i = new Scanner(System.in).nextInt();
         double j = new Scanner(System.in).nextDouble();
-        System.out.print(i+j);
+        System.out.println(i+j);
+        double d = (double)i;
+        System.out.println(d);
+        long l = (long)i;
+        System.out.println(l);
+        l = (long)(i+j);
+        System.out.println(l);
+
+        }
+
     }
-}
